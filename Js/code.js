7d78aaf7ba9e2a75d7b546d2ae69ba25430fded6
Slowstar0010 <starphj007@daum.net>
2023-09-16 22:47:35 +0900

    Add_index_event

--- Js/code.js
+++ Js/code.js
@@ -1,7 +1,19 @@
-const menu_imgs = document.querySelectorAll(".Module_imgs img")
+const articleElements = document.querySelectorAll(".Module_imgs");
 
-for (const menu_img of menu_imgs) {
-    menu_img.addEventListener('click', function(event) {
-      console.log(event)
-    })
-  }
+for (const articleElement of articleElements) {
+    articleElement.addEventListener('click', function(event) {
+        const clickedElement = event.target;
+        const parentArticle = clickedElement.closest('article.Module_imgs');
+
+        if (parentArticle) {
+            const childElements = Array.from(parentArticle.querySelectorAll('.Module_img')); // 이미지와 div 요소 선택
+            const index = childElements.indexOf(clickedElement); // 클릭된 요소의 인덱스 찾기
+
+            if (index !== -1) {
+                console.log('클릭된 요소의 인덱스:', index);
+            } else {
+                console.log('클릭된 요소가 부모 article 안에서 찾을 수 없음.');
+            }
+        }
+    });
+}
