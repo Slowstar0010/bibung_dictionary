f5573897066e265598201a3d0ff0d657861b72cf
Slowstar0010 <starphj007@daum.net>
2023-09-21 16:46:06 +0900

    add_audio_file
    add_audio_file

---
+++ js/lecture.js
@@ -0,0 +1,47 @@
+const imgs = document.querySelectorAll(".Module_imgs");
+let styleElement = null;
+
+const Retune_Btn = document.querySelector(".Retune_Btn")
+
+function flip_div(event) {
+    const clickedClassName = event.target.classList.item(event.target.classList.length - 1);
+
+    if (!styleElement) {
+        styleElement = document.createElement('style');
+        document.head.appendChild(styleElement);
+    }
+
+    styleElement.innerHTML = `
+    .Lecture {
+        display: block;
+    }
+    
+    .${clickedClassName}:hover {
+        display: none;
+    }`;
+
+    return ClickedName = clickedClassName
+
+}
+
+
+function Retune_menu(event) {
+    if (!styleElement) {
+        styleElement = document.createElement('style');
+        document.head.appendChild(styleElement);
+    }
+    styleElement.innerHTML = `
+    .Lecture {
+        display: none;
+    }
+
+    .${ClickedName}:hover {
+        display: flex;
+    }
+
+    }`;
+}
+
+
+imgs.forEach(articleElement => articleElement.addEventListener("click", flip_div));
+Retune_Btn.addEventListener("click", Retune_menu)
